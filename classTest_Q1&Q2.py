68d6dce3a2c6097db747ebbea2b332e8c1cd56d2
mian95 <umairim24@gmail.com>
2024-12-10 09:53:50 +0500

    Uploading quiz of 10 December

---
+++ classTest_Q1&Q2.py
@@ -0,0 +1,16 @@
+# Importing models
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+
+
+x,y = load_wine(return_X_y=True)
+
+print("For the Wine Dataset")
+split_ratios = [0.5, 0.4, 0.3, 0.1]  # Test sizes for 50/50, 60/40, 70/30, 90/10 splits
+
+for test_size in split_ratios:
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
+    print(f"\nFor {int((1-test_size)*100)}/{int(test_size*100)} split_size:")
+    print(f"Testing Set Size: {len(x_test)} Instances")
+    print(f"Training Set Size: {len(x_train)} Instances")
+
---
+++ classTest_Q3.py
@@ -0,0 +1,26 @@
+from sklearn.datasets import load_wine,load_breast_cancer,load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+
+
+
+data_sets = [load_wine,load_breast_cancer,load_digits]  
+
+for data_set in data_sets:
+    x,y=data_set(return_X_y=True)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4)
+
+    model = GaussianNB()
+    """ to train the model"""
+    model.fit(x_train, y_train)
+    
+    """ Calculate accuracies"""
+    train_accuracy = accuracy_score(model.predict(x_train),y_train)
+    test_accuracy = accuracy_score(model.predict(x_test),y_test)
+    
+    """ Print results """
+    print("----------------------------")
+    print(f"Training Accuracy: {train_accuracy}")
+    print(f"Testing Accuracy: {test_accuracy}")
+
